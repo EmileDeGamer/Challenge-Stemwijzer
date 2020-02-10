6595fed52c6673f1424285aa112d66e3f305e703
EmileDeGamer <47303713+EmileDeGamer@users.noreply.github.com>
2020-02-10 12:39:59 +0100

    layout

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log(subjects)
+
+//let parties = document.getElementById('parties')
+//let nonparties = document.getElementById('nonparties')
+
+console.log(parties)
+console.log(nonparties)
